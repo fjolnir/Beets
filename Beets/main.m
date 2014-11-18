b394f2ef193fa92e568fbf41d4e4ed74730e3021
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2014-11-18 23:59:23 +0900

    Blah, of course a reference to the window is required

--- Beets/main.m
+++ Beets/main.m
@@ -1,7 +1,13 @@
 #import <UIKit/UIKit.h>
 
+@interface AppDelegate : NSObject <UIApplicationDelegate>
+@property(nonatomic, retain) UIWindow *window;
+@end
+@implementation AppDelegate
+@end
+
 int main(int argc, char * argv[]) {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, nil);
+        return UIApplicationMain(argc, argv, nil, @"AppDelegate");
     }
 }
